6386de6bf58fa794fadefa8b56da62463a83bf1a
6162201035-dev <6162201035@student.unpar.ac.id>
2025-11-28 11:48:46 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,31 +25,31 @@ with col1:
     with st.container(border=True, height=350): # Buat lebih tinggi
         st.markdown("### 1. 👥 Customer")
         st.markdown("Analisis demografi pelanggan, cuaca, dan *dwell time*.")
-        st.page_link("Dahsboard/pages/1_Customer_Profile.py", label="Buka ➔", use_container_width=True)
+        st.page_link("pages/1_Customer_Profile.py", label="Buka ➔", use_container_width=True)
 
 with col2:
     with st.container(border=True, height=350):
         st.markdown("### 2. 🔗 Relation")
         st.markdown("Analisis hubungan antara area yang saling berasosiasi.")
-        st.page_link("Dahsboard/pages/2_Associated_Area.py", label="Buka ➔", use_container_width=True) 
+        st.page_link("pages/2_Associated_Area.py", label="Buka ➔", use_container_width=True) 
 
 with col3:
     with st.container(border=True, height=350):
         st.markdown("### 3. 🏬 Potency")
         st.markdown("Analisis potensi perilaku pelanggan di setiap area.")
-        st.page_link("Dahsboard/pages/3_Area_Performance.py", label="Buka ➔", use_container_width=True) 
+        st.page_link("pages/3_Area_Performance.py", label="Buka ➔", use_container_width=True) 
 
 with col4:
     with st.container(border=True, height=350):
         st.markdown("### 4. ⏳ Period")
         st.markdown("Analisis pola pelanggan terhadap waktu per jam dan hari.")
-        st.page_link("Dahsboard/pages/4_Time_Period_Traffic_Flow.py", label="Buka ➔", use_container_width=True)
+        st.page_link("pages/4_Time_Period_Traffic_Flow.py", label="Buka ➔", use_container_width=True)
         
 with col5:
     with st.container(border=True, height=350):
         st.markdown("### 5. 🚦 Traffic")
         st.markdown("Analisis traffic dan flow gabungan untuk Area dan Gerbang.")
-        st.page_link("Dahsboard/pages/5_Area_Traffic_Gate_Flow.py", label="Buka ➔", use_container_width=True)
+        st.page_link("pages/5_Area_Traffic_Gate_Flow.py", label="Buka ➔", use_container_width=True)
 
 st.markdown("---") 
 
@@ -58,4 +58,5 @@ st.markdown("""
 <small>© 2025 Data Analysis Dashboard | Dibentuk oleh N .</small>
 
 """, unsafe_allow_html=True)
+
 
